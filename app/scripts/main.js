4a311ee556d6730d3087316495e7217fcf4c39b6
Dane Petersen <thegreatsunra@gmail.com>
2016-03-25 22:58:13 -0700

    Add JS to toggle drawer and overlay visibility on click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
 console.log('\'Allo \'Allo!');
+
+var els;
+
+if (els = document.querySelectorAll(".c-toggle__link")) {
+  for (var i = els.length - 1; i >= 0; i--) {
+    els[i].addEventListener('click', function(e) {
+      e.preventDefault();
+      var target;
+      if (target = document.querySelector(".c-drawer")) {
+        target.classList.toggle("c-drawer--hidden");
+      }
+      var overlay;
+      if (target = document.querySelector(".c-overlay")) {
+        target.classList.toggle("c-overlay--hidden");
+      }
+    })
+  }
+}
