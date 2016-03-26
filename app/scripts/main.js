a8878dbdba1bb74f37f54420206350fc21d8a2a9
Dane Petersen <thegreatsunra@gmail.com>
2016-03-26 16:16:30 -0700

    Refactor functions to support space-delimited class name lists

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,29 @@
 'use strict';
 
 // simple function for toggling classes
-function toggleClass(targetName, className) {
+function toggleClass(targetName, classNames) {
   var target = document.getElementsByClassName(targetName)[0];
   if (target) {
-    target.classList.toggle(className);
+    if (classNames.indexOf(' ')) {
+      // there are multiple class names separated by spaces, so let's split and iterate through them
+      var classes = classNames.split(' ');
+      for (var j = classes.length - 1; j >= 0; j--) {
+        target.classList.toggle(classes[j]);
+      }
+    } else {
+      // there's just one class name, so apply it
+      target.classList.toggle(classNames);
+    }
   }
 }
 
 // toggle drawer-specific classes when drawer toggle is fired
 function toggleDrawer(event) {
   event.preventDefault();
-  toggleClass('js-c-drawer', 'c-drawer--hidden');
+  toggleClass('js-c-drawer', 'c-drawer--hidden c-drawer--narrow@md c-drawer--wide@lg c-drawer--narrow@lg');
   toggleClass('js-c-overlay', 'c-overlay--hidden');
-  toggleClass('js-c-drawer', 'c-drawer--narrow@md');
+  toggleClass('js-c-header', 'c-header--narrow@lg c-header--wide@lg');
+  toggleClass('js-c-view', 'c-view--narrow@lg c-view--wide@lg');
 }
 
 // add event listener to drawer toggle controls
