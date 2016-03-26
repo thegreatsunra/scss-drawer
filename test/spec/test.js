193684267294ac15ff0bdb76079e9704e0b34e1b
Dane Petersen <thegreatsunra@gmail.com>
2016-03-26 07:32:47 -0700

    Write a basic test
    I have no idea what I’m doing.

--- test/spec/test.js
+++ test/spec/test.js
@@ -4,7 +4,8 @@
   describe('Give it some context', function () {
     describe('maybe a bit more context here', function () {
       it('should run here few assertions', function () {
-
+        var answer = 42;
+        answer.should.equal(42);
       });
     });
   });
