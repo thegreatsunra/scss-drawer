48b2cd3ad1d1de062fa5b181531ecc3f26b351b4
Dane Petersen <thegreatsunra@gmail.com>
2016-03-26 07:30:44 -0700

    Lint automatically during development

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -109,7 +109,7 @@ gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
   ]).on('change', reload);
 
   gulp.watch('app/styles/**/*.scss', ['styles']);
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
+  gulp.watch('app/scripts/**/*.js', ['scripts', 'lint']);
   gulp.watch('app/fonts/**/*', ['fonts']);
   gulp.watch('bower.json', ['wiredep', 'fonts']);
 });
